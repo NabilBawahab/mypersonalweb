453129c57264b4b685c7c42f995b1b624fe582b5
Nabil Muhammad Bawahab <nabilbawahab@gmail.com>
2025-01-26 13:04:31 +0700

    Setup awal

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#heading", {
+  strings: [
+    "Fullstack Developer",
+    "Front-End Engineer",
+    "Back-End Engineer",
+    "Musician",
+  ],
+  loop: true,
+  typeSpeed: 70,
+});
